30ef5a99867a0a8a32dcc42153bb448d8a976ab0
Rod.Lipe <rodolfo.engenharia2020@gmail.com>
2023-02-17 22:28:02 -0300

    melhorei a calculadora de boletim

--- helloworld/src/BoletimNotas.java
+++ helloworld/src/BoletimNotas.java
@@ -1,2 +1,21 @@
 public class BoletimNotas {
+
+    public static void main (String [] args){
+
+        int nt1 = 0;
+        int nt2 = 0;
+        int nt3 = 1;
+        int nt4 = 10;
+        int mediaFinal = (nt1 + nt2 + nt3 + nt4)/4;
+        if (mediaFinal > 5)
+            System.out.println("aprovado");
+        else if (mediaFinal == 5)
+            System.out.println("Passou raspando");
+        else
+            System.out.println("reprovado");
+
+        System.out.println(mediaFinal);
+
+
+    }
 }
