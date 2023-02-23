66bd2feb945237d0974c34e0f163294e9f9d9d13
Rod.Lipe <rodolfo.engenharia2020@gmail.com>
2023-02-22 21:17:48 -0300

    algoritmo relacional em WhileLaço

--- helloworld/src/CalculadoraJava/CalculadoraJava.java
+++
@@ -1,7 +0,0 @@
-package CalculadoraJava;
-
-public class CalculadoraJava {
-
-
-
-}
---
+++ helloworld/src/Test/WhileLaço.java
@@ -0,0 +1,19 @@
+package Test;
+
+public class WhileLaço {
+
+    public static void main(String[] args) {
+
+        int nm= 1;
+        System.out.println(nm);
+
+        do {
+            nm++;
+            System.out.println(nm);
+        }while (nm < 100);
+
+        System.out.println("acabou");
+
+
+    }
+}
